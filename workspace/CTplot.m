f45a4cfc4bda88b97b4125864cb7cb0ee91eea80
Dylan-GAO <gaojingbo0620@outlook.com>
2017-09-17 20:18:33 +0800

    x

--- workspace/CTplot.m
+++ workspace/CTplot.m
@@ -1,5 +1,24 @@
-function CTplot(OriData, Theta)
-ImgData = rebuild(OriData, Theta);
+function CTplot(OriData, Theta, x0, y0, d, option)
+
+if option == 1
+    oc = sqrt(x0^2 + y0^2);
+    koc = y0 / x0;
+
+    for i = 1 : size(OriData, 2)
+        tmp = round(oc * sin(Theta(i) - atan(abs(koc))) / d);
+        if tmp >= 0
+            for j = size(OriData, 1) : -1 : 1 + tmp
+                OriData(j, i) = OriData(j - tmp, i);
+            end
+        else
+            for j = 1 : size(OriData, 1) + tmp
+                OriData(j, i) = OriData(j - tmp, i);
+            end
+        end
+    end
+end
+
+ImgData = rebuild(OriData, rad2deg(Theta));
 figure
 imagesc(ImgData);
 end
