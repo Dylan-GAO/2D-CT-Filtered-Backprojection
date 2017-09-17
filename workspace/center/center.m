aebd117accc8f6380e8d0d4b5cdcb1422fd6e929
Dylan-GAO <gaojingbo0620@outlook.com>
2017-09-18 00:53:22 +0800

    x

--- workspace/center/center.m
+++ workspace/center/center.m
@@ -92,6 +92,7 @@ y_cen = res(2);
 fprintf('x_cen = %f, y_cen = %f\n', x_cen, y_cen);
 
 % plot
+x = -15 : 0.1 : 15;
 plot(x,sqrt((1-x.^2/15^2)*40^2));
 plot(x,-sqrt((1-x.^2/15^2)*40^2));
 scatter(x_cen, y_cen);
--- workspace/d.m
+++ workspace/d.m
@@ -1,10 +1,10 @@
 data = data_2;
 data(:, 14 : 108) = 0;
 for i = 1 : 13
-data(1 : 370, i) = 0;
+    data(1 : 370, i) = 0;
 end
 for i = 109 : 180
-data(112 : 512, i) = 0;
+    data(112 : 512, i) = 0;
 end
 b=[];
 for i = 1 : 180
