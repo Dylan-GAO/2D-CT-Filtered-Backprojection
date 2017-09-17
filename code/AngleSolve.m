9b5f6ee12d7c57b77b5cb4979aedf3d8af16948c
Xuchengzhe <xuchengzheGithub@163.com>
2017-09-17 18:15:13 +0800

    www

--- code/AngleSolve.m
+++ code/AngleSolve.m
@@ -1,15 +1,8 @@
-function angle_result = AngleSolve(projection_scale)
-%projrction_scale = S(ellipse)/S(circle), angles used degree.
+function angle = AngleSolve(s1)
 syms pro_angle
 syms equ
-equ = (5.*abs(tan(pro_angle)).*sqrt(64+9.*(tan(pro_angle)).^2))./(4.*(1+(tan(pro_angle)).^2)) - projection_scale;
-angle_pre = solve(equ,'pro_angle');
-k = 1;
-angle_result = [];
-for i = 1:size(angle_pre,1)
-    if ~isreal(angle_pre(i))
-        angle_result(k) = atan(imag(angle_pre(i))/real(angle_pre(i)));
-        k = k+1;
-    end
-end
+equ = 10*sqrt(((64+9*(tan(pro_angle))^2))/(1+(tan(pro_angle))^2)) - s1;
+angle_result = roundn(eval(solve(equ,'pro_angle')),-4);
+%angle_result = rad2deg(angle_result)
+angle = abs(angle_result(1));
 end
